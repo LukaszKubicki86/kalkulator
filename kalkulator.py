a92c9ac3b5376b8ccf9c75670080c51cb54ff8da
Łukasz Kubicki <lkubicki1986@gmail.com>
2022-10-08 07:56:25 +0200

    funkcje kalkulatora

---
+++ kalkulator.py
@@ -0,0 +1,16 @@
+#  funkcja dodawania
+def dodawanie (x, y):
+    return x + y
+
+# funkcja odejmowania
+def odejmowanie(x, y):
+    return x - y
+
+# funkcja mnozenia
+def mnozenie (x, y):
+    return x * y
+
+def dzielenie (x, y):
+    return x / y
+
+print ("to jest to")
